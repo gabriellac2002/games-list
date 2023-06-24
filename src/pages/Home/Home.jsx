a327e4c831477bde4bd103b3d997ef38b8c8fda2
cidadcruz cida <gabriellacsilva2002@gmail.com>
2023-06-24 16:57:13 -0300

    estulizando pesquisas

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,12 +9,16 @@ import logo from '../../assets/logo.png';
 //components
 import Card from '../../components/Card/card.jsx';
 import Navbar from '../../components/navbar/navbar.jsx';
+import Select from 'react-select'
+import ReactLoading from 'react-loading';
 
 const Home = () =>  {
 
   const [games,setGames] = useState([]);
   const [itensPerPage, setItensPerPage] = useState(9);
   const [currentPage,setCurrentPage] = useState(0);
+  const [selectedGenre, setSelectedGenre] = useState(null); 
+  const [searchQuery, setSearchQuery] = useState('');
 
   const pages = Math.ceil(games.length / itensPerPage) ;
   const startIndex = currentPage * itensPerPage;
@@ -29,8 +33,24 @@ const Home = () =>  {
     }).then((res) => res.json().then(data => setGames(data)))
   },[]);
 
-    
+  const filteredGamesForName = games.filter((game) =>
+    game.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
+  const filteredGames = selectedGenre
+    ? games.filter((game) => game.genre.includes(selectedGenre))
+  : games;
+  
+  const displayedGames = selectedGenre
+    ? filteredGames.slice(startIndex, endIndex)
+    : filteredGamesForName.slice(startIndex, endIndex);
+
+
+  const uniqueGenres = Array.from(new Set(games.map((game) => game.genre)));
+  const options = uniqueGenres.map((genre) => ({
+    value: genre,
+    label: genre
+  }));
 
   return (
     <div className='body_page'>
@@ -53,14 +73,38 @@ const Home = () =>  {
 
         <div className='list_games'>
           <div className='titulo_list_games'>
-              <h2>Listagem de jogos</h2>
-              
+            <h2>Listagem de jogos</h2>
+          </div>
+
+          <div className='pesquisas'>
+
+            <div className='select'>
+              <Select 
+                options={options} 
+                value={selectedGenre}
+                onChange={(selectedOption) =>
+                  setSelectedGenre(selectedOption ? selectedOption.value : null)
+                }
+              />
+            </div>
+
+            <div className='searchbar'>
+              <input
+                className='input_search'
+                type="text"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                placeholder="Digite o nome do jogo"
+              />
+            </div>
+
           </div>
 
           <div className='container_cards'>
-            {Array.isArray(games) ? currentItens.map((game) => (
-              <Card title={game.title} thumbnail={game.thumbnail} short_description={game.short_description} />
-            )) : <h1>Loading ...</h1>} 
+          {Array.isArray(displayedGames) ? displayedGames.map((game) => (
+            <Card title={game.title} thumbnail={game.thumbnail} short_description={game.short_description} />
+          )) : <ReactLoading type={'spin'} color='#2D6BEA' height={'20%'} width={'20%'} />}
+
           </div>
           
           <div className='pagination'>
